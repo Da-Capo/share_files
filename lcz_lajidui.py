aeab3a1c47160dec5d8111396540f4a36cc4b2aa
Peter Hu <Da-Capo@users.noreply.github.com>
2019-01-07 17:16:05 +0800

    Update lcz_lajidui.py
    add changed seresnext

--- lcz_lajidui.py
+++ lcz_lajidui.py
@@ -18,8 +18,6 @@ labels = ['LCZ1-Compact high-rise ',
 
 
 
-
-
 #@title models
 import torch.nn as nn
 import torch.nn.functional as F
@@ -400,7 +398,110 @@ def se_resnext101_32x4d(num_classes):
     model = SE_ResNeXt(baseWidth=4, cardinality=32, head7x7=False, layers=(3, 4, 23, 3), num_classes=num_classes)
     return model
   
-  
+ 
+class SE_ResNeXt_DOUBLE(nn.Module):
+    def __init__(self, input_col=10, outputsize=128,baseWidth=4, cardinality=32, head7x7=False, layers=(2, 2, 2, 2), num_classes=17):
+        """ Constructor
+        Args:
+            baseWidth: baseWidth for SE_ResNeXt.
+            cardinality: number of convolution groups.
+            layers: config of layers, e.g., [3, 4, 6, 3]
+            num_classes: number of classes
+        """
+        super(SE_ResNeXt, self).__init__()
+        block = SEBottleneck
+
+        self.cardinality = cardinality
+        self.baseWidth = baseWidth
+        self.num_classes = num_classes
+        self.inplanes = 64
+
+        self.head7x7 = head7x7
+        if self.head7x7:
+            self.conv1 = nn.Conv2d(input_col, 64, 7, 2, 3, bias=False)
+            self.bn1 = nn.BatchNorm2d(64)
+        else:
+            self.conv1 = nn.Conv2d(input_col, 32, 3, 2, 1, bias=False)
+            self.bn1 = nn.BatchNorm2d(32)
+            self.conv2 = nn.Conv2d(32, 32, 3, 1, 1, groups=8, bias=False)
+            self.bn2 = nn.BatchNorm2d(32)
+            self.conv3 = nn.Conv2d(32, 64, 3, 1, 1, groups=16, bias=False)
+            self.bn3 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], 2)
+        self.layer3 = self._make_layer(block, 256, layers[2], 2)
+        self.layer4 = self._make_layer(block, 512, layers[3], 2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512 * block.expansion, outputsize)
+        self.fc1 = nn.Linear(outputsize, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, 0.0001)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        """ Stack n bottleneck modules where n is inferred from the depth of the network.
+        Args:
+            block: block type used to construct SE_ResNeXt
+            planes: number of output channels (need to multiply by block.expansion)
+            blocks: number of blocks to be built
+            stride: factor to reduce the spatial dimensionality in the first bottleneck of the block.
+        Returns: a Module consisting of n sequential bottlenecks.
+        """
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, self.baseWidth, self.cardinality, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, self.baseWidth, self.cardinality))
+            
+        layers.append(nn.Dropout(0.2))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.head7x7:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+        else:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+            x = self.conv2(x)
+            x = self.bn2(x)
+            x = self.relu(x)
+            x = self.conv3(x)
+            x = self.bn3(x)
+            x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = self.fc1(x)
+        return x
+
+
 
 import torch
 import torch.nn as nn
@@ -861,6 +962,11 @@ class Helper:
             model = se_resnext50_32x4d(num_classes=num_classes).to(device)
         if name == "seresnext101":
             model = se_resnext101_32x4d(num_classes=num_classes).to(device)
+        if name == "seresnext26_sen1":
+            model = SE_ResNeXt_DOUBLE(input_col=8, outputsize=128, num_classes=num_classes).to(device)
+        if name == "seresnext26_sen2":
+            model = SE_ResNeXt_DOUBLE(input_col=10, outputsize=128, num_classes=num_classes).to(device)
+            
         return model, -1
 
     def load_model(self, rule):
